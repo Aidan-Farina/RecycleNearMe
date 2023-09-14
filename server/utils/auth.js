0e5d8cfc06adc81c9d04dd5382c20118e2b7f0a8
Aidan <farinaaidan2@gmail.com>
2023-09-13 23:23:54 -0400

    stupid stuff not working

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,8 @@
 const { GraphQLError } = require("graphql");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const secret = import.meta.env.PW_SECRET_HASH;
+const secret = process.env.PW_SECRET_HASH;
 const expiration = "2h";
 
 module.exports = {
